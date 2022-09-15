7b7a027459dc6a9a5f690c76e634a55a7116472a
tingtinghsu <tingtinghsu.tw@gmail.com>
2022-09-16 00:21:53 +0800

    Day2: defn vs def

--- src/tutorial/core.clj
+++ src/tutorial/core.clj
@@ -2,8 +2,14 @@
   (:gen-class))
 
 (defn -main
-  [& args]
-  (println "
- Hello World, Tingting!"))
+  [number]
+  (println "hello world, Tingting!")
+  (println "I bought"  number  "harry potter series"))
 
+(def harry-potter-series
+  ["the Philosopher's Stone" "the Chamber of Secrets" "the Prisoner of Azkaban"])
 
+(defn x [a b] (+ a b))
+;;  (x 2 3) => 5
+(def y (fn [a b] (+ a b)))
+;;  (y 2 3) => 5
